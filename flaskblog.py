e1aca9a1bf12b5d84176c1db96375c2ca2afac8d
“rajeshcskumar” <rajeshkumar1612169@gmail.com>
2025-03-30 17:20:10 +0500

    add forms

---
+++ flaskblog.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '913cb850f85ba16a5068ed3befc229f0'
+app.config()
+
+posts = [
+    {
+        'author': 'Corey Shagner',
+        'title': 'blog post1',
+        'content': 'first blog conent',
+        'date_posted': 'April 20 2025'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'blog post2',
+        'content': 'first blog conent 2',
+        'date_posted': 'April 21 2025'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', posts=posts,title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title="Register", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
