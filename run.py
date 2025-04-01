5679d773f5737afcabc0e691ffeb4ef16c7fd909
“rajeshcskumar” <rajeshkumar1612169@gmail.com>
2025-04-01 17:23:17 +0500

    login feature

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from flaskblog import app, db
 with app.app_context():
     db.create_all()
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
