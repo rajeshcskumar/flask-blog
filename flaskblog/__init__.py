8a2b01733e316e087980f0c5d53d74c6c3717421
“rajeshcskumar” <rajeshkumar1612169@gmail.com>
2025-04-03 23:48:13 +0500

    reset password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '913cb850f85ba16a5068ed3befc229f0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -10,5 +12,10 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'rajeshkumar1612169@gmail.com'
+app.config['MAIL_PASSWORD'] = 'didmsmpvgxtbtqbo'
+mail = Mail(app)
 from flaskblog import routes
